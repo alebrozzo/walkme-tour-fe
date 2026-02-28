05adb85c794c97cc600cc526e786d41249e0f7b5
Alejandro Brozzo <alejandro.brozzo@wunderdog.fi>
2026-02-28 21:02:36 +0100

    fixed intall issue

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,4 @@
 const tseslint = require('typescript-eslint');
-const reactHooks = require('eslint-plugin-react-hooks');
 const prettierConfig = require('eslint-config-prettier');
 
 module.exports = tseslint.config(
@@ -7,6 +6,5 @@ module.exports = tseslint.config(
     ignores: ['node_modules/**', 'dist/**', '.expo/**', 'web-build/**', 'eslint.config.js'],
   },
   ...tseslint.configs.recommended,
-  reactHooks.configs.flat.recommended,
   prettierConfig,
 );
